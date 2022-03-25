11eba13c1a5a026946dd46fe77082da04f3adc69
kornellewy <40360922+kornellewy@users.noreply.github.com>
2022-03-25 08:23:12 +0100

    final multiple types gen and his data

--- tests/generators/multiple_types_generator_test.py
+++ tests/generators/multiple_types_generator_test.py
@@ -10,12 +10,6 @@ class PLType3IdcardGeneratorTests(unittest.TestCase):
     def setUp(self) -> None:
         self.dataset_path = "data/multiple_types"
 
-    # def test_generate_idcard(self):
-    #     generator = PLType3IdcardGenerator()
-    #     id_card_front_img, id_card_back_img = generator.generate_idcard()
-    #     self.assertIsInstance(id_card_front_img, ImageType)
-    #     self.assertIsInstance(id_card_back_img, ImageType)
-
     def test_generate_idcard(self):
         generator = MultipleTypesGenerator(dataset_path=self.dataset_path)
         id_card_img = generator.generate_idcard()
